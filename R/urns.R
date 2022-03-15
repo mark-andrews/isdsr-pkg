aa5c1f5425c014531ee84f46968eb85404cf30de
Mark Andrews <mjandrews.org@gmail.com>
2022-03-15 20:59:43 +0000

    Add binomial problem Bayesian tools
    For plotting likelihood function, posteriors, etc.

--- R/urns.R
+++ R/urns.R
@@ -194,3 +194,258 @@ binomial_confint <- function(sample_size, observed, level = 0.95) {
   magrittr::set_names(conf_interval, c(alpha/2, 1-alpha/2))
 }
 
+
+
+
+#' Plot the likelihood function of a binomial model
+#'
+#' @export
+#' @param m Number of successes
+#' @param n Number of trials
+#' @param lim x-axis limits
+#' @param interval Show likelihood interval? If NULL, no interval, if numeric,
+#'   show likelihood interval 1/2^interval
+#' @return A ggplot object
+#' @examples
+#' binomial_likelihood(250, 135)
+#' @import tibble
+#' @import ggplot2
+binomial_likelihood_plot <- function(m, n, lim = c(0, 1), interval = NULL){
+  
+  find_roots <- function(m, n, z=1/4) {
+    
+    N <- n
+    n <- m
+    
+    likelihood <- function(x, N, n){
+      x^n * (1-x)^(N-n)
+    }
+    
+    f <- function(x, N, n, z) {
+      standardized.likelihood <- likelihood(x, N, n) / likelihood(n/N, N, n)
+      standardized.likelihood - z
+    }
+    
+    left_root <- uniroot(f, 
+                         c(0, n/N), 
+                         tol = 0.0001, 
+                         N = N, 
+                         n=n,
+                         z=z)
+    
+    right_root <- uniroot(f, 
+                          c(n/N, 1), 
+                          tol = 0.0001, 
+                          N = N, 
+                          n=n,
+                          z=z)
+    
+    list(x = left_root$root,
+         y = likelihood(left_root$root, N, n),
+         xend = right_root$root,
+         yend = likelihood(right_root$root, N, n))
+  }
+  
+  data_df <- tibble(x = seq(lim[1], lim[2], length.out = 1000),
+                    y = x^m * (1-x)^(n-m))
+  
+
+  
+  p1 <- data_df %>%
+    ggplot(aes(x = x, y = y)) +
+    geom_line() +
+    labs(x = latex2exp::TeX("$\\theta$"),
+         y = latex2exp::TeX("$L(\\theta | n, m)$")) +
+    #ggtitle(sprintf("%d successes in %d trials", m, n)) +
+    theme_classic() +
+    theme(axis.text.y = element_blank(),
+          axis.ticks.y = element_blank()) 
+  
+  if (is.null(interval)){
+    p1
+  } else {
+    
+    roots <- find_roots(m, n, z = 1/2^interval)
+    
+    p1 + geom_segment(aes(x = roots$x,
+                          y = roots$y,
+                          xend = roots$xend,
+                          yend = roots$yend),
+                      col='red')
+  }
+
+}
+
+#' Plot a Beta distribution
+#'
+#' @param alpha First shape parameter of the Beta distribution.
+#' @param beta Second shape parameter of the Beta distribution.
+#' @param show_hpd Show the HPD interval as a line segment.
+#' @param level Amount of mass in the HPD interval.
+#' @return A ggplot object.
+#' @import tibble
+#' @import ggplot2
+#' @examples
+#' beta_plot(2, 2)
+#' beta_plot(10, 5)
+#' @export
+beta_plot <- function(alpha, beta, show_hpd = FALSE, level = 0.95){
+  
+  data_df <- tibble(x = seq(0, 1, length.out = 1000),
+                    y = dbeta(x, shape1 = alpha, beta))
+  
+  p <- data_df %>%
+    ggplot(aes(x = x, y = y)) +
+    geom_line() +
+    labs(x = latex2exp::TeX("$\\theta$"),
+         y = latex2exp::TeX("$P(\\theta)$")) +
+    ggtitle(sprintf("Beta(%2.1f, %2.1f)", alpha, beta)) +
+    theme_classic()
+  
+  if (show_hpd){
+    hpd_interval <- get_beta_hpd(alpha, beta, level = level)
+    p + geom_segment(aes(x = hpd_interval$lb,
+                         xend = hpd_interval$ub,
+                         y = hpd_interval$p_star,
+                         yend = hpd_interval$p_star),
+                     colour = 'red',
+                     size = 0.5,
+                     data = NULL)
+  } else {
+    p
+  }
+  
+}
+
+#' Plot the posterior distribution of a binomial model with Beta prior
+#'
+#' @param n Number of trials
+#' @param m Number of successes
+#' @param alpha First shape parameters of the Beta prior
+#' @param beta Second shape parameter of the Beta prior
+#' @param show_hpd Show the HPD interval
+#' @param level The amount of mass in HPD interval
+#' @return
+#' @examples
+#' binomial_posterior_plot(250, 139, 5, 5)
+#' binomial_posterior_plot(100, 60, 2, 2, level = 0.99)
+#' @export
+binomial_posterior_plot <- function(n, m, alpha, beta, show_hpd = FALSE, level = 0.95){
+  beta_plot(m + alpha, n - m + beta, show_hpd = show_hpd, level = level)
+}
+
+#' The high posterior density interval of a Beta distribution.
+#' 
+#' @param alpha First shape parameter of the Beta distribution.
+#' @param beta Second shape parameter of the Beta distribution.
+#' @return A list with lower and upper bound of the HPD interval, and minimum density of interval.
+#' @examples
+#' get_beta_hpd(10, 5)
+#' @export
+get_beta_hpd <- function(alpha, beta, level = 0.95){
+  # This will break if either alpha < 1.0 or beta < 1.0
+  # or alpha = beta = 1.0
+  stopifnot(alpha >= 1.0, beta >= 1.0, !((alpha == 1) & (beta ==1)))
+  
+  interval_mass <- function(p_star){
+    # Return the area under the curve for the
+    # set of points whose density >= p_star.
+    
+    f <- function(val){
+      d <- dbeta(val, alpha, beta)
+      if (d >= p_star){
+        return(d)
+      }
+      else {return(0)}
+    }
+    
+    return(integrate(Vectorize(f), 0.0, 1.0)$value)
+    
+  }
+  
+  err_fn <- function(p_star, hpd_mass=level){
+    (hpd_mass-interval_mass(p_star))^2
+  }
+  
+  max_f <- dbeta((alpha-1)/(alpha+beta-2), alpha, beta)
+  
+  Q <- optimize(err_fn,
+                interval = c(0, max_f)
+  )
+  
+  precision <- 3
+  p_star <- round(Q$minimum, precision)
+  
+  inside <- FALSE
+  for (x in seq(0.0, 1.0, by=10^(-precision-1))) {
+    if (round(dbeta(x, alpha, beta), precision) >= p_star) {
+      if (inside){
+        stop_interval <- x
+      } else {
+        start_interval <- x
+        inside <- TRUE
+      }
+    } else
+    {
+      if (inside){
+        inside <- FALSE
+        interval <- c(start_interval, stop_interval)
+      }
+    }
+  }
+  
+  list(lb = interval[1],
+       ub = interval[2],
+       p_star = p_star)
+  
+}
+
+#' The high posterior density interval in a binomial problem with Beta prior.
+#' 
+#' @param m Number of successes
+#' @param n Number of trials
+#' @param alpha First shape parameter of the Beta prior distribution.
+#' @param beta Second shape parameter of the Beta prior distribution.
+#' @return A vector with lower and upper bound of the HPD interval.
+#' @examples
+#' binomial_posterior_hpd(172, 1300, 1, 1)
+#' @export
+binomial_posterior_hpd <- function(m, n, alpha, beta, level = 0.95){
+  hpd_interval <- get_beta_hpd(m + alpha, n - m + beta, level = level)
+  
+  c(hpd_interval$lb, hpd_interval$ub)
+}
+
+#' Summary statistics of a Beta distribution
+#'
+#' @param alpha First shape parameter of the Beta distribution
+#' @param beta Second shape parameter of the Beta distribution
+#' @return A list with summary statistics of the Beta distribution
+#' @examples
+#' beta_summary(3, 5)
+#' @export
+beta_summary <- function(alpha, beta){
+  mean <- alpha/(alpha+beta)
+  var <- (alpha*beta)/( (alpha+beta)^2 * (alpha + beta + 1))
+  sd <- sqrt(var)
+  mode <- (alpha - 1)/(alpha + beta - 2)
+  
+  list(mean = mean,
+       var = var,
+       sd = sqrt(var),
+       mode = mode)
+}
+
+#' Summary stats of posterior distribution of binomial model with Beta prior
+#'
+#' @param n Number of trials
+#' @param m Number of successes
+#' @param alpha First shape parameter of the Beta prior
+#' @param beta Second shape parameter of the Beta prior
+#' @return A list with summary statistics of the posterior distribution
+#' @examples
+#' binomial_posterior_summary(3, 5)
+#' @export
+binomial_posterior_summary <- function(n, m, alpha, beta){
+  beta_summary(m + alpha, n - m + beta)
+}
