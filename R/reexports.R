adca7106a592cf01194fca12cfee052add50b988
Mark Andrews <mjandrews.org@gmail.com>
2025-07-10 10:57:21 +0100

    Update reexports; rexport moments

--- R/reexports.R
+++ R/reexports.R
@@ -1,3 +1,11 @@
 #' @importFrom skimr skim
 #' @export
 skimr::skim
+
+#' @importFrom moments skewness
+#' @export
+moments::skewness
+
+#' @importFrom moments kurtosis
+#' @export
+moments::kurtosis
