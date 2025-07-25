f15c27a7e32fe382bcb28b3552824be3b22af7d5
Mark Andrews <mjandrews.org@gmail.com>
2025-07-25 09:49:04 +0100

    Reexport effectsize::cohens_d and hedges_g

--- R/reexports.R
+++ R/reexports.R
@@ -5,3 +5,11 @@ skimr::skim
 #' @importFrom moments skewness
 #' @export
 moments::skewness
+
+#' @importFrom effectsize hedges_g
+#' @export
+effectsize::hedges_g
+
+#' @importFrom effectsize cohens_d
+#' @export
+effectsize::cohens_d
