7cf6f55461ad6a417b23828491fe246414458f6b
Mark Andrews <mjandrews.org@gmail.com>
2025-08-10 11:58:20 +0100

    Droplevels from gap52

--- data-raw/gap52.R
+++ data-raw/gap52.R
@@ -1,6 +1,8 @@
 ## code to prepare `gap52` dataset goes here
 
 data(package = "gapminder")
-gap52 <- gapminder |> dplyr::filter(year == 1952, continent != "Oceania")
+gap52 <- gapminder |>
+  dplyr::filter(year == 1952, continent != "Oceania") |>
+  droplevels()
 
 usethis::use_data(gap52, overwrite = TRUE)
